b0c5aa42d1c6addb72be870efcf1d2800691489d
Azat <279733@gmail.com>
2019-07-07 23:53:42 +0200

    add preloader

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,20 +2,32 @@
  * Section: jQuery
  */
 
+
 $(function() {
 	
-	$photo = $('.c-main__photo');
-	
-	setTimeout(function() {
-		$photo.addClass('js-first')
-	},2500);
-	
-	$photo.on('click', function() {
-		$(this).removeClass('js-first').addClass('js-second');
+	$(window).on('load', function () {
+		var $preloader = $('#page-preloader'),
+			$spinner   = $preloader.find('.spinner');
 		setTimeout(function() {
-			$photo.addClass('hidden')
-		},1170)
-	})
+			$spinner.fadeOut();
+			$preloader.delay(800).fadeOut('slow');
+		},1800)
+	});
+	
+		$photo = $('.c-main__photo');
+		
+		$photo.addClass('js-first');
+		
+		setTimeout(function() {
+			$photo.addClass('js-second');
+		}, 2500);
+		
+		$photo.on('click', function() {
+			$(this).removeClass('js-second').addClass('js-third');
+			setTimeout(function() {
+				$photo.addClass('hidden');
+			}, 1170);
+		});
 	
 	//@@include('global/_global.js')
 	//@@include('components/_components.js')
