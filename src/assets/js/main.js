18c183f9194af747431144ce387d89fe33ba4cce
Azat <279733@gmail.com>
2019-07-11 00:53:03 +0200

    fix animate on click in photo

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -15,22 +15,25 @@ $(function() {
 	
 		$photo = $('.c-main__photo');
 		
-		//$photo.addClass('js-first');
-		
-		setTimeout(function() {
-			$photo.addClass('js-second');
-		});
 		
 		setTimeout(function photoRemote() {
 			$photo.removeClass('js-second');
 		}, 4500);
-		
+	
+	//* Photo on click
 		$photo.on('click', function() {
-			$(this).addClass('js-first');
+			if ($(this).hasClass('js-first')){
+			// Can I leave this blank?
+			}
+			else {
+				$(this).addClass('js-first');
+			}
+			
 			setTimeout(function photoRemote() {
 				$photo.removeClass('js-first');
-			}, 1500);
+			}, 670);
 		});
+		//* END of: Photo on click
 	
 	$('.c-main__social').mouseover(function() {
 		$('.h-first').removeClass('h-first');
